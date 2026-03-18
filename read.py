aa46e2c5a2867399dd61479e7d31d20aed947c58
Ashvitha <ashvitha@gmail.com>
2026-03-18 23:33:46 +0530

    Updated damage detection using TopHat + improved Canny

--- read.py
+++ read.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 
 # Read image
-img = cv.imread("photos/car12.jpg")
+img = cv.imread("photos/car14.jpeg")
 cv.imshow("Original", img)
 
 # Create mask
@@ -24,14 +24,11 @@ mask2 = np.where((mask==2)|(mask==0), 0, 1).astype('uint8')
 # Extract car
 car = img * mask2[:,:,np.newaxis]
 
-# Blur background
-blur = cv.GaussianBlur(img,(25,25),0)
-
 # Combine
-result = np.where(mask2[:,:,None]==1, img, blur)
+#result = np.where(mask2[:,:,None]==1, img, blur)
 
 cv.imshow("Segmented Car", car)
-cv.imshow("Blurred Background", result)
+#cv.imshow("Blurred Background", result)
 
 # Convert to grayscale
 gray = cv.cvtColor(car, cv.COLOR_BGR2GRAY)
@@ -42,9 +39,23 @@ tophat = cv.morphologyEx(gray, cv.MORPH_TOPHAT, kernel)
 
 cv.imshow("Scratch Enhancement", tophat)
 
+# Blur background
+#blur = cv.GaussianBlur(img,(25,25),0)
+
+#smooth the tophat image
+tophat_blur= cv.GaussianBlur(tophat,(3,3),0)
+#Edge detection(fixed)
+edges =cv.Canny(tophat_blur,20,80)
+
+
 # Edge detection
-blur_gray = cv.GaussianBlur(gray,(9,9),0)
-edges = cv.Canny(blur_gray,100,200)
+#blur_gray = cv.GaussianBlur(gray,(9,9),0)
+#edges = cv.Canny(blur_gray,100,200)
+#edges=cv.Canny(tophat,20,80)
+
+#Thicken edges
+kernel = np.ones((3,3), np.uint8)
+edges = cv.dilate(edges, kernel, iterations=2)
 
 cv.imshow("Edges", edges)
 
@@ -56,7 +67,7 @@ damage = img.copy()
 for cnt in contours:
     area = cv.contourArea(cnt)
 
-    if area > 300:
+    if area > 50:
         x,y,w,h = cv.boundingRect(cnt)
         cv.rectangle(damage,(x,y),(x+w,y+h),(0,0,255),2)
 
